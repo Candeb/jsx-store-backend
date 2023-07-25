ecaab66c9339015cb8fbdd98d9ba41aa2176023a
Candeb <candebustos084@gmail.com>
2023-07-25 18:59:46 -0300

    prueba deploy

--- app.ts
+++ app.ts
@@ -23,45 +23,12 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-  <meta
-  http-equiv="Content-Security-Policy"
-  content="default-src 'self'; img-src https://*; child-src 'none';" />
-
     <title>Funcionando</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      html {
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
   </head>
   <body>
-    <section>
+    <p>
       funcionando!
-    </section>
+    </p>
   </body>
 </html>
 `;
