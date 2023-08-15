d3fd55e2a8954d7e8b8f4dc54b7605a13d374796
Candeb <candebustos084@gmail.com>
2023-08-14 21:41:16 -0300

    add cors

--- app.ts
+++ app.ts
@@ -5,11 +5,13 @@ import { authRouter } from './src/auth/authRouter';
 import { productRouter } from './src/product/productRouter';
 import { brandsRouter } from './src/brands/brandsRouter';
 import { orderRouter } from './src/order/orderRouter';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 3002;
-const app = express();
-
-app.use(express.json());
 
 app.use('/auth', authRouter);
 app.use('/product', productRouter);
@@ -20,6 +22,7 @@ const server = app.listen(port, () => {
   createPrismaClient();
   getConfig();
   console.log(`Server running on port ${port}!`);
+  console.log(`CORS-enabled web server listening on port ${port}`);
 });
 
 server.keepAliveTimeout = 120 * 1000;
