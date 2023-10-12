c7fedbd3aa5e0dbb09cf720e34fa0905987d3851
Candeb <candebustos084@gmail.com>
2023-10-12 11:37:50 -0300

    fix deploy

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,31 +2,37 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { getConfig } from '../config/config';
 
-//AUTENTICACION
+// AUTENTICACIÓN
 export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const header = req.headers.authorization;
+
   if (!header) {
-    res.status(401).json({ message: 'No autorizado: no se encontró el token' });
-    return;
+    return res
+      .status(401)
+      .json({ message: 'No autorizado: no se encontró el token' });
   }
+
   const token = header.split(' ')[1];
+
   try {
     const data = jwt.verify(token, getConfig().accesTokenSecret);
+
     if (data) {
       res.locals.email = (data as any).email;
       res.locals.userId = (data as any).userId;
       next();
-      return;
+    } else {
+      res.status(401).json('No autorizado: el token no es válido');
     }
   } catch (err: any) {
     if (err.name === 'TokenExpiredError') {
       res.status(401).json('No autorizado: el token expiró');
+    } else {
+      res.status(401).json('No autorizado: el token no es válido');
     }
-    res.status(401).json('No autorizado: el token no es valido');
   }
-  res.status(401).json('No autorizado: el token no es valido');
 };
